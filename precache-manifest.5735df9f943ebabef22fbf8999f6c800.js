0984a94771514fe25afb99ee148694478e169fc2
Alexandru Hohota <alex.hohota@gmail.com>
2020-04-05 19:50:08 +0300

    Updates

---
+++ precache-manifest.5735df9f943ebabef22fbf8999f6c800.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e752e415c3226d46bb45bc350a305dd",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "dffe4f42ab0b47ddc169",
+    "url": "/corona-tracker/static/css/main.24f726bc.chunk.css"
+  },
+  {
+    "revision": "74b87a56e0d0175e93cf",
+    "url": "/corona-tracker/static/js/2.cc6a57fc.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.cc6a57fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dffe4f42ab0b47ddc169",
+    "url": "/corona-tracker/static/js/main.896ab73a.chunk.js"
+  },
+  {
+    "revision": "a393508f62a73a5533e7",
+    "url": "/corona-tracker/static/js/runtime-main.85aff16f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
